6d549a15d4df462a41604e6330ecd5b9a75e10c8
Henrique Fazzio Badin <henrique.badin@gmail.com>
2025-04-29 16:01:58 -0300

    Add id attribute to projects div for improved accessibility

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -7,7 +7,7 @@ import { SiAdobephotoshop } from "react-icons/si";
 
 export function Projects() {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-10 px-4">
+        <div id="projects" className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-10 px-4">
             <CardProjeto
             image="/assets/estoca-ai.png"
             href="https://www.google.com/"
