2ba74a69accce73e1ed55bc8eb860044f972e98e
Henrique Fazzio Badin <henrique.badin@gmail.com>
2025-04-30 18:38:37 -0300

    Passando as imagens de logo para png

--- src/components/partner-components/carousel-plugin.tsx
+++ src/components/partner-components/carousel-plugin.tsx
@@ -4,12 +4,13 @@ import * as React from "react"
 import { PartnerLogo } from "./partner-logo";
 
 const LogoList = [
-    "https://media.licdn.com/dms/image/v2/D4D0BAQFPtz92o_eL1Q/company-logo_200_200/company-logo_200_200/0/1711374204983/inteli_game_lab_logo?e=1750896000&v=beta&t=WSuZ3MdZwI65gF3y3rDKQiHZOu3vqL5wKeFtR4-4bZo",
+    "/assets/intelli-gamelab.jpeg",
     "/assets/finq-logo.png",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhIBanYASx6yTAfvw3mXKk5RsUgKlqLgPOTw&s",
-    "https://media.licdn.com/dms/image/v2/C4D1BAQE9lkIEfmcHeQ/company-background_10000/company-background_10000/0/1650376636950/insper_in_hesalth_cover?e=1746316800&v=beta&t=bV8BXFtkWMtiogv2sui-6ptFf9P0texg_9vF43r4XvE",
-    "https://www.insper.edu.br/content/insper-portal/pt/cursos/graduacao/organizacoes-estudantis/_jcr_content/root/responsivegrid/wrapper/container_grid/container/wrapper_copy_copy_co_1469664296/container_grid/container/image.coreimg.png/1733768250709/insper-sec-150x150.png",
-    "https://ibs-americas.com/assets/img/AIC-IBS.png",
+    "/assets/fund-estudar.jpeg",
+    "/assets/in-health.jpeg",
+    "/assets/ibs-americas.jpeg",
+    "/assets/insper-sec.jpeg",
+    "/assets/na-pratica.jpeg",
 ]
 
 export function CarouselPlugin() {
